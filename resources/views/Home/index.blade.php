cd7aa2e97e2617d06997af1c0f23c5bd26e772d6
cy123 <974020129@qq.com>
2016-09-17 00:59:04 +0800

    modify a litttle

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -7,7 +7,7 @@
 
 @section('scrollme')
 <section id="hero" class="scrollme">
-    <div class="container-fluid element-img" style="background: url(public/Home/img/2.png) no-repeat center center fixed;background-size: cover">
+    <div class="container-fluid element-img" style="background: url(public/Home/img/1.jpeg) no-repeat center center fixed;background-size: cover">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 vertical-align cover boost text-center">
                 <div class="center-me animateme" data-when="exit" data-from="0" data-to="0.6" data-opacity="0" data-translatey="100">
@@ -39,7 +39,7 @@
 	     <div class="row">
             <div class="col-md-8 col-md-offset-2 opening-statement">
                 <div class="col-md-4">
-                    <h3><a href="post/37/index.html">涓�寮� CD 鐨勫姩鐢�</a></h3>
+                    <h3><a href="post/37/index.html">小站正在完善中...</a></h3>
                     <span>
                         <span class="post-meta">
 						  <time datetime="2016-05-10T08:01:05.000Z" itemprop="datePublished">
@@ -52,8 +52,7 @@
                 </div>
                 <div class="col-md-8">
                     <p>
-                        涓や釜 Div 瀹炵幇鐨勫姩鐢�12&lt;div class="cd"&gt;&lt;/div&gt;&lt;div class="music"&gt;&lt;/div&gt;
-						涓轰粈涔堣鍐欒繖涓憿锛熺瓟妗堟槸 鐜╂病閿� 灏辨槸鐜┾�﹀彲鑳借剳鎶藉惂鍏跺疄鎴戝彂鐜� CSS 鍔ㄧ敾鐪熺殑寰堝ソ鐜╋紒鍙互瀹炵幇涓�浜涚偒閰风殑鏁堟灉鎺ヤ笅鏉ユ垜浼氱户缁啓鍑犱釜濂界帺鐨� Demo琛ュ厖涓嬫垜骞朵笉鍏呭疄鐨� CSS3 鍔ㄧ敾鐭ヨ瘑鈥︾帺骞跺揩涔愮殑瀛︿範~浠ｇ爜璇锋埑涓嬮潰http://codepen.io/_kieran/p
+
                     </p>
                     
                     <p class="pull-right readMore">
@@ -66,7 +65,7 @@
             </div>
         </div>
         <nav class="pagination" role="pagination">
-          <a class="pull-right" href="page/2/index.html">Older Posts 鈫�</a>
+          <a class="pull-right" href="page/2/index.html">Older Posts ...</a>
         </nav>
     </div>
 </section>        
